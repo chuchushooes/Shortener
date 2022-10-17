54f9c049c47948cbb267d9458a53b5b3462c95a2
chuchushooes <chuchushooes@gmail.com>
2022-10-17 23:09:38 +0800

    refactor: modify mongoose config

--- utilities/copyUrl.js
+++
@@ -1,8 +0,0 @@
-function copyUrl() {
-  let copyText = document.getElementById('copy')
-  console.log(`hi`)
-  copyText.select()
-  copyText.setSelectionRange(0, 999999)
-  navigator.clipboard.writeText(copyText.value);
-  alert("Copied the Url: " + copyText.value);
-}
